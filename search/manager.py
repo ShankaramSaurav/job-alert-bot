717c6d6e6f2127176520a27c647cae52ef1c0df4
ShankaramSaurav <66571183+ShankaramSaurav@users.noreply.github.com>
2026-08-02 14:47:48 +0530

    Implement search_all function to aggregate job listings

---
+++ search/manager.py
@@ -0,0 +1,22 @@
+import json
+
+from search.greenhouse import search as greenhouse
+from search.lever import search as lever
+
+
+def search_all():
+
+    all_jobs = []
+
+    with open("data/companies.json") as f:
+        companies = json.load(f)
+
+    # Search Greenhouse companies
+    for company in companies["greenhouse"]:
+        all_jobs.extend(greenhouse(company))
+
+    # Search Lever companies
+    for company in companies["lever"]:
+        all_jobs.extend(lever(company))
+
+    return all_jobs
