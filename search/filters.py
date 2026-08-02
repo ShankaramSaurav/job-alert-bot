076675e8084b839e1612b08d597dd4f6b8558038
ShankaramSaurav <66571183+ShankaramSaurav@users.noreply.github.com>
2026-08-02 11:54:57 +0530

    Add preferred locations for job filtering
    Added preferred locations for job search filters.

--- search/filters.py
+++ search/filters.py
@@ -1,33 +1,37 @@
 ROLES = [
 
 "Senior Data Engineer",
-
 "Staff Data Engineer",
-
 "Principal Data Engineer",
-
 "Lead Data Engineer",
-
 "Data Platform Engineer",
-
 "Analytics Engineer",
-
 "Data Warehouse Engineer"
-
+  
 ]
 
 IGNORE = [
 
 "Intern",
-
 "Manager",
-
 "Director",
-
 "Sales",
-
 "Marketing",
-
 "Finance"
 
+]
+
+PREFERRED_LOCATIONS = [
+    "india",
+    "bangalore",
+    "bengaluru",
+    "hyderabad",
+    "pune",
+    "gurugram",
+    "gurgaon",
+    "noida",
+    "delhi",
+    "mumbai",
+    "chennai",
+    "remote",
 ]
