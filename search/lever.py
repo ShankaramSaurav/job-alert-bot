534833d6a93f7def9d0432b32df33884d1650065
ShankaramSaurav <66571183+ShankaramSaurav@users.noreply.github.com>
2026-08-02 11:12:41 +0530

    Implement job search function for Lever API
    Added a function to search for job postings from Lever API.

--- search/lever.py
+++ search/lever.py
@@ -1,1 +1,37 @@
+import requests
 
+BASE="https://api.lever.co/v0/postings/{company}?mode=json"
+
+def search(company):
+
+    jobs=[]
+
+    try:
+
+        r=requests.get(BASE.format(company=company),timeout=20)
+
+        if r.status_code!=200:
+
+            return jobs
+
+        for job in r.json():
+
+            jobs.append({
+
+                "company":company.title(),
+
+                "title":job["text"],
+
+                "location":job["categories"]["location"],
+
+                "url":job["hostedUrl"],
+
+                "platform":"Lever"
+
+            })
+
+    except:
+
+        pass
+
+    return jobs
