40f7ffa8d7870c864a29394b6645a21491fd6816
ShankaramSaurav <66571183+ShankaramSaurav@users.noreply.github.com>
2026-08-02 11:11:58 +0530

    Add roles and ignore lists to filters.py

--- search/filters.py
+++ search/filters.py
@@ -1,1 +1,33 @@
+ROLES = [
 
+"Senior Data Engineer",
+
+"Staff Data Engineer",
+
+"Principal Data Engineer",
+
+"Lead Data Engineer",
+
+"Data Platform Engineer",
+
+"Analytics Engineer",
+
+"Data Warehouse Engineer"
+
+]
+
+IGNORE = [
+
+"Intern",
+
+"Manager",
+
+"Director",
+
+"Sales",
+
+"Marketing",
+
+"Finance"
+
+]
