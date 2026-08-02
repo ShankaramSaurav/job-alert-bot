10be8ca5600fed1762b41065cfb7bb6519373f9c
ShankaramSaurav <66571183+ShankaramSaurav@users.noreply.github.com>
2026-08-02 11:20:16 +0530

    Add keyword extraction functionality

--- utils/keyword_extractor.py
+++ utils/keyword_extractor.py
@@ -1,1 +1,42 @@
 
+import re
+
+KEYWORDS = [
+
+"Snowflake",
+"SQL",
+"Python",
+"AWS",
+"Azure",
+"GCP",
+"Spark",
+"Kafka",
+"Airflow",
+"DBT",
+"Databricks",
+"ETL",
+"ELT",
+"Data Warehouse",
+"Power BI",
+"Tableau",
+"Docker",
+"Kubernetes",
+"Git",
+"CI/CD"
+
+]
+
+
+def extract(text):
+
+    found = []
+
+    lower = text.lower()
+
+    for word in KEYWORDS:
+
+        if re.search(rf"\b{re.escape(word.lower())}\b", lower):
+
+            found.append(word)
+
+    return found
