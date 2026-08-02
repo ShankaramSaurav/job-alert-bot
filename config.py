65422207c4091b20921b501d0edab39993b5bd46
ShankaramSaurav <66571183+ShankaramSaurav@users.noreply.github.com>
2026-08-02 10:37:45 +0530

    Initialize search keywords and company data
    Added search keywords and company URLs for job listings.

---
+++ config.py
@@ -0,0 +1,21 @@
+SEARCH_KEYWORDS = [
+    "Senior Data Engineer",
+    "Lead Data Engineer",
+    "Staff Data Engineer",
+    "Principal Data Engineer",
+]
+
+COMPANIES = [
+    {
+        "name": "Adobe",
+        "url": "https://careers.adobe.com/us/en/search-results"
+    },
+    {
+        "name": "Atlassian",
+        "url": "https://www.atlassian.com/company/careers/all-jobs"
+    },
+    {
+        "name": "Microsoft",
+        "url": "https://jobs.careers.microsoft.com"
+    },
+]
